4e15cc4bc1978868be2a0e33bc571ee5a070e47e
Nuno Queirós <nuno_queiros_60@hotmail.com>
2025-11-25 05:21:36 +0000

    ClientMain - faz pedido ao ServidorDiretoriaMain para obter o ip e porta do Servidor Principal

--- src/pt/isec/pd/tp/Client/ClientMain.java
+++ src/pt/isec/pd/tp/Client/ClientMain.java
@@ -1,5 +1,47 @@
 package pt.isec.pd.tp.Client;
 
+import pt.isec.pd.tp.Client.Comunicacao.ClientComunicacao;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class ClientMain {
+
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("Uso: java ClienteMain <IP_Diretoria> <Porto_UDP>");
+            return;
+        }
+
+        try {
+
+            InetAddress dirIp = InetAddress.getByName(args[0]);
+            int dirPort = Integer.parseInt(args[1]);
+
+            ClientComunicacao clcom = new ClientComunicacao(dirIp, dirPort);
+
+            String[] serverDetails = clcom.requestPrincipalServer();
+
+            if (serverDetails != null) {
+                String serverIp = serverDetails[0];
+                int serverTcpPort = Integer.parseInt(serverDetails[1]);
+
+                System.out.println("Servidor Principal encontrado: " + serverIp + ":" + serverTcpPort);
+
+                // TODO: LIGAR AO SERVIDOR PRINCIPAL
+            } else {
+                System.out.println("Não foi possível obter um servidor principal.");
+            }
+
+        } catch (UnknownHostException e) {
+            // 2. O código que lida com o erro (tratamento da exceção)
+            System.err.println("Erro: O endereço IP/Host '" + args[0] + "' é inválido ou desconhecido.");
+            System.exit(1); // Terminar o programa
+        } catch (NumberFormatException e) {
+            System.err.println("Erro: O porto fornecido ('" + args[1] + "') não é um número válido.");
+            System.exit(1);
+        }
+    }
 
 }
