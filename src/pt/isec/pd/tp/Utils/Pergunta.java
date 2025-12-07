057a4eeed5cc0a671e47f530cb8527eab99c2530
Nuno Queirós <nuno_queiros_60@hotmail.com>
2025-12-07 19:11:28 +0000

    Pergunta - adicionado mais get e set

--- src/pt/isec/pd/tp/Utils/Pergunta.java
+++ src/pt/isec/pd/tp/Utils/Pergunta.java
@@ -11,11 +11,20 @@ public class Pergunta implements Serializable {
     private int opcaoCorreta;
     private int idDocente;
     private String codigo;
+    private String dataInicio;
+    private String dataFim;
+    private int totalRespostas;
+    private double percentagemCertas;
+
 
     public Pergunta(String enunciado, String[] opcoes, int opcaoCorreta) {
         this.enunciado = enunciado;
         this.opcoes = opcoes;
         this.opcaoCorreta = opcaoCorreta;
+    }
+
+    public Pergunta() {
+        // Default constructor
     }
 
     // Getters
@@ -25,6 +34,11 @@ public class Pergunta implements Serializable {
     public int getOpcaoCorreta() { return opcaoCorreta; }
     public int getIdDocente() { return idDocente; }
     public String getCodigo() { return codigo; }
+    public String getDataInicio() { return dataInicio; }
+    public String getDataFim() { return dataFim; }
+    public int getTotalRespostas() { return totalRespostas; }
+    public double getPercentagemCertas() { return percentagemCertas; }
+
 
     // Setters
     public void setId(int id) { this.id = id; }
@@ -33,6 +47,11 @@ public class Pergunta implements Serializable {
     public void setOpcaoCorreta(int opcaoCorreta) { this.opcaoCorreta = opcaoCorreta; }
     public void setIdDocente(int idDocente) { this.idDocente = idDocente; }
     public void setCodigo(String codigo) { this.codigo = codigo; }
+    public void setDataInicio(String dataInicio) { this.dataInicio = dataInicio; }
+    public void setDataFim(String dataFim) { this.dataFim = dataFim; }
+    public void setTotalRespostas(int totalRespostas) { this.totalRespostas = totalRespostas; }
+    public void setPercentagemCertas(double percentagemCertas) { this.percentagemCertas = percentagemCertas; }
+
 
     @Override
     public String toString() {
